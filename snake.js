cb1b83c1c3337a8b14e3032a03c55f518e629706
baconmcdouble <sihantang0316@gmail.com>
2023-01-06 22:58:28 -0500

    Added onload function

--- snake.js
+++ snake.js
@@ -3,3 +3,12 @@ var rows = 20;
 var cols = 20;
 var board;
 var context;
+
+window.onload = function () {
+    board = document.getElementById('board');
+    board.height = rows * boxSize;
+    board.width = cols * boxSize;
+    context = board.getContext("2d");
+
+    update();
+}
