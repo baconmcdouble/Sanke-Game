1d2cfbaf90837b326c433afcbb84e4404f77baa3
baconmcdouble <sihantang0316@gmail.com>
2023-01-10 16:56:37 -0500

    fixed typo

--- snake.js
+++ snake.js
@@ -1,19 +1,19 @@
-var boxSize = 25;
+var blockSize = 25;
 var rows = 20;
 var cols = 20;
 var board;
 var context;
 
-var snakeX = boxSize * 5;
-var snakeY = boxSize * 5;
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
 
-var foodX = boxSize * 10;
-var foodY = boxSize * 10;
+var foodX = blockSize * 10;
+var foodY = blockSize * 10;
 
 window.onload = function () {
     board = document.getElementById('board');
-    board.height = rows * boxSize;
-    board.width = cols * boxSize;
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
     context = board.getContext("2d");
 
     update();
@@ -24,8 +24,8 @@ function update() {
     context.fillRect(0, 0, board.width, board.height);
 
     context.fillStyle = '#A1B57D';
-    context.fillRect(snakeX, snakeY, boxSize, boxSize);
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
 
     context.fillStyle = '#B33030';
-    context.fillRect(foodX, foodY, boxSize, boxSize);
+    context.fillRect(foodX, foodY, blockSize, blockSize);
 }
