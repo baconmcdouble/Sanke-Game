93c5b1e6d1ea24c74bac1da32b5f0540aeb52f22
baconmcdouble <sihantang0316@gmail.com>
2023-01-11 18:13:45 -0500

    Added velocity to snake

--- snake.js
+++ snake.js
@@ -7,6 +7,9 @@ var context;
 var snakeX = blockSize * 5;
 var snakeY = blockSize * 5;
 
+var velocityX = 0;
+var velocityY = 0;
+
 var foodX = blockSize * 10;
 var foodY = blockSize * 10;
 
@@ -16,16 +19,48 @@ window.onload = function () {
     board.width = cols * blockSize;
     context = board.getContext("2d");
 
-    update();
+    // No parentheses when passing function as a parameter!!!
+    placeFood();
+    document.addEventListener('keyup', changeDirection);
+    setInterval(update, 1000 / 10)
 }
 
 function update() {
     context.fillStyle = '#D3ECA7';
     context.fillRect(0, 0, board.width, board.height);
 
-    context.fillStyle = '#A1B57D';
-    context.fillRect(snakeX, snakeY, blockSize, blockSize);
-
     context.fillStyle = '#B33030';
     context.fillRect(foodX, foodY, blockSize, blockSize);
+
+    if (snakeX == foodX && snakeY == foodY) {
+        placeFood();
+    }
+
+    context.fillStyle = '#A1B57D';
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+
+}
+
+function changeDirection(e) {
+    if (e.code == 'ArrowUp' && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    } else if (e.code == 'ArrowDown' && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    } else if (e.code == 'ArrowRight' && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+    } else if (e.code == 'ArrowLeft' && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    }
+}
+
+function placeFood() {
+    foodX = Math.floor(Math.random() * cols) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
 }
